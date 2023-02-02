99da6675a73d452199c8c5628f2ad27d296a709b
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2023-02-02 10:59:08 +0800

    Update main.py

--- cog/main.py
+++ cog/main.py
@@ -3,26 +3,7 @@ import discord
 from discord.ext import commands
 from discord.commands import slash_command
 
-class main(commands.Cog):
-    def __init__(self,bot):
-        self.bot=bot
-    option=discord.Option
-
-    nhentai=discord.SlashCommandGroup("nhentai","( ͡° ͜ʖ ͡°)")
-    @nhentai.command()
-    async def random_num(self,ctx):
-        random_int=random.randint(1,999999)
-        await ctx.respond(f'https://nhentai.net/g/{random_int}/')
-
-    @nhentai.command()
-    async def godnum(self,ctx,num:int,page:option(int,"page",required = False)):
-        await ctx.respond(f'https://nhentai.net/g/{num}/{page}')
-
-    @nhentai.command()
-    async def parody(self,ctx,parody:str):
-        await ctx.respond(f"https://nhentai.net/parody/{parody}/")
-
-    class tag(discord.ui.View):
+class tag(discord.ui.View):
         @discord.ui.select( 
         tag = "Choose a tag!", # the placeholder text that will be displayed if nothing is selected
         min_values = 1, # 最少幾個選項
@@ -52,32 +33,35 @@ class main(commands.Cog):
             discord.SelectOption(label="shibari",description="綁縛"),
             discord.SelectOption(label="orgasm denial",description="禁止高潮"),
             discord.SelectOption(label="human pet",description="寵物玩法"),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
-            discord.SelectOption(label="",description=""),
             
-
-            
-        ]
+            ]
         )
         async def select_callback(self, select, interaction): # the function called when the user is done selecting options
             await interaction.response.send_message(f"https://nhentai.net/tag/{select.values[0]}/")
+
+class main(commands.Cog):
+    def __init__(self,bot):
+        self.bot=bot
+
+    
+    option=discord.Option
+
+    nhentai=discord.SlashCommandGroup("nhentai","( ͡° ͜ʖ ͡°)")
     @nhentai.command()
-    async def flavor(self,ctx):
+    async def random_num(self,ctx):
+        random_int=random.randint(1,999999)
+        await ctx.respond(f'https://nhentai.net/g/{random_int}/')
+
+    @nhentai.command()
+    async def godnum(self,ctx,num:int,page:option(int,"page",required = False)):
+        await ctx.respond(f'https://nhentai.net/g/{num}/{page}')
+
+    @nhentai.command()
+    async def parody(self,ctx,parody:str):
+        await ctx.respond(f"https://nhentai.net/parody/{parody}/")
+    
+    @nhentai.command()
+    async def tag(self,ctx):
         await ctx.respond("Choose a flavor!",view=tag())
 
     @slash_command(name="ping",description="just ping")
