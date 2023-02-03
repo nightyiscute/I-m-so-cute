2036b20b96ce69e00af4de3e246e25a11ec98934
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2023-02-03 17:03:05 +0800

    Update main.py

--- cog/main.py
+++ cog/main.py
@@ -1,56 +1,54 @@
 import random
 import discord
 import json
+import datetime
 from discord.ext import commands
 from discord.commands import slash_command
 
-with open('data.json',mode='r',encoding='utf8')as jfile:
-    jdata=json.load(jfile)
-
-for i in range(143):
-    a=i
-
-class tag(discord.ui.View):
-        @discord.ui.select( 
-        tag = "Choose a tag!", # the placeholder text that will be displayed if nothing is selected
-        min_values = 1, # 最少幾個選項
-        max_values = 1, # 最多幾個選項
-        options = [ # 選項
-            discord.SelectOption(label=jdata[i][0],description=jdata[i][1]),
-            ]
-        )
-        async def select_callback(self, select, interaction): # the function called when the user is done selecting options
-            await interaction.response.send_message(f"https://nhentai.net/tag/{select.values[0]}/")
 
 class main(commands.Cog):
-
     def __init__(self,bot):
         self.bot=bot
     option=discord.Option
-
-    nhentai=discord.SlashCommandGroup("nhentai","( ͡° ͜ʖ ͡°)")
-    @nhentai.command()
-    async def random_num(self,ctx):
-        random_int=random.randint(1,999999)
-        await ctx.respond(f'https://nhentai.net/g/{random_int}/')
-
-    @nhentai.command()
-    async def godnum(self,ctx,num:int,page:option(int,"page",required = False)):
-        await ctx.respond(f'https://nhentai.net/g/{num}/{page}')
-
-    @nhentai.command()
-    async def parody(self,ctx,parody:str):
-        await ctx.respond(f"https://nhentai.net/parody/{parody}/")
-    
-    @nhentai.command()
-    async def tag(self,ctx):
-        await ctx.respond("Choose a flavor!",view=tag())
 
     @slash_command(name="ping",description="just ping")
     async def ping(self,ctx):
         await ctx.respond(f"{round(ctx.bot.latency*1000)}(ms)")
     
-    
+    @slash_command(name="luck",description="text your luck")
+    async def luck(self,ctx):
+        random_int=random.randint(0,100)
+        if random_int<10:
+            random_luck="大凶"
+        if 10<=random_int<30:
+            random_luck="兇"
+        if 30<=random_int<70:
+            random_luck="中"
+        if 70<=random_int<90:
+            random_luck="吉"
+        if 90<=random_int:
+            random_luck="大吉"
+
+        if random_luck=="大吉":
+            lcolor=0x00ff1e
+            word="恭喜!今天適合買大樂透"
+        if random_luck=="吉":
+            lcolor=0x00e1ff
+            word="恭喜!今天會有小確幸"
+        if random_luck=="中":
+            lcolor=0xfbff00
+            word="今天運氣不好也不壞，是個平常的一天呢"
+        if random_luck=="兇":
+            lcolor=0xff8800
+            word="今天走路要看路喔，小心踩到狗屎"
+        if random_luck=="大凶":
+            lcolor=0xff0000
+            word="小心血光之災!"
+        embed=discord.Embed(title=random_luck,  color=lcolor,timestamp=datetime.datetime.now())
+        embed.set_author(name="今日運氣")
+        embed.add_field(name=word,value="以上就是占卜結果", inline=False)
+        embed.set_footer(text=f"{ctx.author.display_name}占卜於")
+        await ctx.respond(embed=embed)
 
     
 def setup(bot):
