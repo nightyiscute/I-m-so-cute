092548e42da294b4a919acc93d2b59f5a477227b
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2023-02-11 14:00:39 +0800

    Update n.py

--- cog/n.py
+++ cog/n.py
@@ -4,27 +4,29 @@ import discord
 from discord.ext import commands
 from discord.commands import slash_command
 
-
 with open('data.json',mode='r',encoding='utf8')as jfile:
     jdata=json.load(jfile)
 
 for i in range(143):
     a=i
-    
+
 class tag(discord.ui.View):
     @discord.ui.select( 
-    tag = "Choose a tag!", # the placeholder text that will be displayed if nothing is selected
+    tag = "Choose a tag!", # 沒選選項時
     min_values = 1, # 最少幾個選項
     max_values = 1, # 最多幾個選項
     options = [discord.SelectOption(label=jdata[a][0],description=jdata[a][1])])
-    async def select_callback(self, select, interaction): # the function called when the user is done selecting options
+    async def select_callback(self, select, interaction): #有選選項時
         await interaction.response.send_message(f"https://nhentai.net/tag/{select.values[0]}/")
+
 
 class n(commands.Cog):
     def __init__(self,bot):
         self.bot=bot
     option=discord.Option
     nhentai=discord.SlashCommandGroup("nhentai","( ͡° ͜ʖ ͡°)")
+    
+
 
     @nhentai.command()
     async def random_num(self,ctx):
