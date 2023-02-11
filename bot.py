171331e55f48fb3bedd966d4b1877bc03b6cb280
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2023-02-11 14:00:26 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,10 +10,10 @@ bot=commands.Bot(command_prefix="!",intents=discord.Intents.all())
 for file in os.listdir('./cog'):
     if file.endswith('.py'):
         try:
-            bot.load_extension(f'cog.{file[:-3]}')
+            bot.load_extension(f'cog.{file[:-3]}',recursive=True)
             print(f'{file}加載成功')
         except Exception as error:
-            print(f'修{file}時間')
+            print(f'修{file}時間:{error}')
 
 token=jdata["token"]
 bot.run(token)
