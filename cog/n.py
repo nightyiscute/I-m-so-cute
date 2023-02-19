75714900788e9e2e0a5e29754afd3f2873141d91
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2023-02-19 12:19:27 +0800

    Update n.py

--- cog/n.py
+++ cog/n.py
@@ -31,23 +31,9 @@ class n(commands.Cog):
         await ctx.respond(f"https://nhentai.net/parody/{parody}/")
     
     @nhentai.command()
-    async def tag(self,ctx):
-        for i in range(143):
-            a=i
-
-        tag=discord.ui.Select(
-            select_type=discord.ComponentType.string_select,
-            placeholder="Choose a tag!", # 沒選選項時
-            min_values = 1, # 最少幾個選項
-            max_values = 1, # 最多幾個選項
-            options = [discord.SelectOption(label=jdata[a][0],description=jdata[a][1])])
-
-        async def select_callback(Select, interaction): #有選選項時
-            await interaction.response.send_message(f"https://nhentai.net/tag/{Select.values[0]}/")
-        tag.callback=select_callback
-        view=View(timeout=0)
-        view.add_item(tag)
-        await ctx.respond("Choose a flavor!",view=tag())
+    async def tag(self,ctx,tag:str):
+        await ctx.respond(f"https://nhentai.net/tag/{tag}/")
+    
 
 def setup(bot):
     bot.add_cog(n(bot))
