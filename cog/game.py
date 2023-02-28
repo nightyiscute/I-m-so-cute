90e16544de97fa49b4b590000039169721500dd8
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2023-02-28 19:08:51 +0800

    Update game.py

--- cog/game.py
+++ cog/game.py
@@ -8,22 +8,22 @@ class game(commands.Cog):
     def __init__(self,bot):
         self.bot=bot
 
-@slash_command(name="比大小",description="比你和機器人誰比較大(?")
-async def 比大小(self,ctx):
-    rannum1=random.randint(0,100)
-    rannum2=random.randint(0,100)
-    if rannum1>rannum2:
-        embed=discord.Embed(title="You win!!", color=0x02e328)
-        embed.add_field(name=f"你的數字:{rannum1}", value=f"對方的數字:{rannum2}",inline=False)
-        await ctx.respond(embed=embed)
-    elif rannum2>rannum1:
-        embed=discord.Embed(title="You lose!!", color=0xe31102)
-        embed.add_field(name=f"你的數字:{rannum1}", value=f"對方的數字:{rannum2}", inline=False)
-        await ctx.respond(embed=embed)
-    else:
-        embed=discord.Embed(title="平手?!", color=0x30a4df)
-        embed.add_field(name=f"你的數字:{rannum1}", value=f"對方的數字:{rannum2}", inline=False)
-        await ctx.respond(embed=embed)
+    @slash_command(name="比大小",description="比你和機器人誰比較大(?")
+    async def 比大小(self,ctx):
+        rannum1=random.randint(0,100)
+        rannum2=random.randint(0,100)
+        if rannum1>rannum2:
+            embed=discord.Embed(title="You win!!", color=0x02e328)
+            embed.add_field(name=f"你的數字:{rannum1}", value=f"對方的數字:{rannum2}",inline=False)
+            await ctx.respond(embed=embed)
+        elif rannum2>rannum1:
+            embed=discord.Embed(title="You lose!!", color=0xe31102)
+            embed.add_field(name=f"你的數字:{rannum1}", value=f"對方的數字:{rannum2}", inline=False)
+            await ctx.respond(embed=embed)
+        else:
+            embed=discord.Embed(title="平手?!", color=0x30a4df)
+            embed.add_field(name=f"你的數字:{rannum1}", value=f"對方的數字:{rannum2}", inline=False)
+            await ctx.respond(embed=embed)
 
 
 def setup(bot):
