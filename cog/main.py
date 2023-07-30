0f85296deef529a840e72bed8558250f308a7961
nightyiscute <109859767+nightyiscute@users.noreply.github.com>
2023-07-30 13:49:32 +0800

    Update main.py

--- cog/main.py
+++ cog/main.py
@@ -1,5 +1,6 @@
 import random
 import discord
+import json
 import datetime
 from discord.ext import commands
 from discord.commands import slash_command
@@ -12,43 +13,49 @@ class main(commands.Cog):
     option=discord.Option
     
     
-
-    
     
     @slash_command(name="luck",description="text your luck")
     async def luck(self,ctx):
-        random_int=random.randint(0,100)
-        if random_int<10:
-            random_luck="大凶"
-        elif 10<=random_int<30:
-            random_luck="兇"
-        elif 30<=random_int<70:
-            random_luck="中"
-        elif 70<=random_int<90:
-            random_luck="吉"
-        elif 90<=random_int:
-            random_luck="大吉"
+        with open('PlayerData.json', 'r', encoding='utf-8') as jdata:
+            data = json.load(jdata)
+        if data.get("userid") is None:
+            await ctx.respond("請先成立帳戶")
+        else:
+            if data.get("luck")>0:
+                await ctx.respond(embed=embed)
+            else:
+                random_int=random.randint(0,100)
+                if random_int<10:
+                    random_luck="大凶"
+                elif 10<=random_int<30:
+                    random_luck="兇"
+                elif 30<=random_int<70:
+                    random_luck="中"
+                elif 70<=random_int<90:
+                    random_luck="吉"
+                elif 90<=random_int:
+                    random_luck="大吉"
 
-        if random_luck=="大吉":
-            lcolor=0x00ff1e
-            word="恭喜!今天適合買大樂透"
-        elif random_luck=="吉":
-            lcolor=0x00e1ff
-            word="恭喜!今天會有小確幸"
-        elif random_luck=="中":
-            lcolor=0xfbff00
-            word="今天運氣不好也不壞，是個平常的一天呢"
-        elif random_luck=="兇":
-            lcolor=0xff8800
-            word="今天走路要看路喔，小心踩到狗屎"
-        elif random_luck=="大凶":
-            lcolor=0xff0000
-            word="小心血光之災!"
-        embed=discord.Embed(title=random_luck, color=lcolor, timestamp=datetime.datetime.now())
-        embed.set_author(name="今日運氣")
-        embed.add_field(name=word,value="以上就是占卜結果", inline=False)
-        embed.set_footer(text=f"{ctx.author.display_name}占卜於")
-        await ctx.respond(embed=embed)
+                if random_luck=="大吉":
+                    lcolor=0x00ff1e
+                    word="恭喜!今天適合買大樂透"
+                elif random_luck=="吉":
+                    lcolor=0x00e1ff
+                    word="恭喜!今天會有小確幸"
+                elif random_luck=="中":
+                    lcolor=0xfbff00
+                    word="今天運氣不好也不壞，是個平常的一天呢"
+                elif random_luck=="兇":
+                    lcolor=0xff8800
+                    word="今天走路要看路喔，小心踩到狗屎"
+                elif random_luck=="大凶":
+                    lcolor=0xff0000
+                    word="小心血光之災!"
+                embed=discord.Embed(title=random_luck, color=lcolor, timestamp=datetime.datetime.now())
+                embed.set_author(name="今日運氣")
+                embed.add_field(name=word,value="以上就是占卜結果", inline=False)
+                embed.set_footer(text=f"{ctx.author.display_name}占卜於")
+                await ctx.respond(embed=embed)
 
     @slash_command(name='dice',description='dice number')
     async def dice(self,ctx,number:int):
@@ -98,8 +105,11 @@ class main(commands.Cog):
     
     @slash_command(name="tag",description="可以用這個tag人")
     async def tag(self,ctx,somebody:discord.Member,times:int):
-        for i in range(0,times):
-            await ctx.respond (f"{somebody.mention}",delete_after=1)
+        if times>20:
+            await ctx.respond("冷靜一點啊")
+        else:
+            for i in range(0,times):
+                await ctx.respond(f"{somebody.mention}",delete_after=1)
             
         
 
